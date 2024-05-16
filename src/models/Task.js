bd935c4725fae652a2bc11e9ee91f22ca59f7939
lapinoux02 <paulbnt@free.fr>
2024-05-16 22:13:10 +0200

    optimize tasks part 1

--- src/models/Task.js
+++ src/models/Task.js
@@ -1,4 +1,4 @@
-import { isSameDay, isToday, isAfter, differenceInDays, subDays, addDays, startOfMonth, addMonths } from 'date-fns'
+import { isSameDay, isToday, isAfter, differenceInDays, subDays, addDays, startOfMonth, addMonths, subMonths } from 'date-fns'
 import { v4 } from 'uuid'
 
 const newDate = (date = new Date()) => new Date(new Date(date).toDateString())
@@ -6,6 +6,7 @@ const nthMonthDay = (date, nth, day) => {
 	const firstDay = startOfMonth(newDate(date))
 	return addDays(firstDay, (day - firstDay.getDay() + 7) % 7 + nth * 7)
 }
+const isAfterOrSame = (date1, date2) => isAfter(date1, date2) || isSameDay(date1, date2)
 
 class Task {
 	constructor({task, categoryId, date, report, taskType, groupId}) {
@@ -41,7 +42,7 @@ class SimpleTask extends Task {
 	}
 
 	get lastTimeToDo() {
-		return newDate(this.done || this.date) // done
+		return newDate(this.done || this.date)
 	}
 
 	display(d) {
@@ -115,25 +116,22 @@ class WeeklyTask extends Task {
 		this.done = done.map(newDate)
 	}
 
-	get lastTimeToDo() { // done
-		let previousDay = [...this.weekDays, ...this.weekDays.map(day => day + 7)].sort((a, b) => (a - b) > 0 ? -1 : 1).find(day => day < (newDate().getDay() + 7)) % 7
-		let previousDate = subDays(newDate(), (newDate().getDay() - previousDay + 7) % 7)
-
-		if (isAfter(this.date, previousDate)) return
-
-		return previousDate
+	get lastTimeToDo() {
+		const lastTimeToDo = newDate()
+		while(this.weekDays.every(wd => lastTimeToDo.getDay() !== wd)) {
+			lastTimeToDo.setDate(lastTimeToDo.getDate() - 1)
+		}
+		return lastTimeToDo
 	}
 
 	display(d) {
 		let date = newDate(d)
 		if (isAfter(this.date, date)) return false
 
-		if (this.done.some(e => isSameDay(newDate(e), date))) {
-			return true
-		} else if (this.report && this.lastTimeToDo && !this.isDone(this.lastTimeToDo) && isSameDay(date, newDate())) {
-			return true
-		} else if (this.report && this.lastTimeToDo && !this.isDone(this.lastTimeToDo) && isSameDay(date, this.lastTimeToDo)) {
-			return false
+		const nextTimeToDo = addDays(newDate(), 1)
+		while(this.weekDays.every(wd => nextTimeToDo.getDay() !== wd)) nextTimeToDo.setDate(nextTimeToDo.getDate() + 1)
+		if (this.report && !this.isDone(this.lastTimeToDo) && (isAfterOrSame(date, this.lastTimeToDo)) && isAfter(nextTimeToDo, date)) {
+			return isSameDay(date, newDate())
 		} else {
 			return this.weekDays.some(weekDay => weekDay === date.getDay())
 		}
@@ -166,7 +164,7 @@ class MonthlyTask extends Task {
 		this.date = newDate(startDate)
 	}
 
-	get lastTimeToDo() { // done
+	get lastTimeToDo() {
 		let lastTimeToDo = newDate()
 		if (this.dayOfMonth > lastTimeToDo.getDate()) {
 			lastTimeToDo.setMonth(lastTimeToDo.getMonth() - 1)
@@ -179,13 +177,8 @@ class MonthlyTask extends Task {
 		let date = newDate(d)
 		if (isAfter(this.date, date)) return false
 
-		if (this.done.some(e => isSameDay(newDate(e), date))) {
-			return true
-		} else if (!this.report) {
-			return date.getDate() === this.dayOfMonth
-		} else if (this.isDone(this.lastTimeToDo)) {
-			return date.getDate() === this.dayOfMonth
-		} else if ((isAfter(date, this.lastTimeToDo) || isSameDay(date, this.lastTimeToDo)) && isAfter(addMonths(this.lastTimeToDo, 1), date)) {
+		const nextTimeToDo = addMonths(this.lastTimeToDo, 1)
+		if (this.report && !this.isDone(this.lastTimeToDo) && (isAfterOrSame(date, this.lastTimeToDo)) && isAfter(nextTimeToDo, date)) {
 			return isSameDay(date, newDate())
 		} else {
 			return date.getDate() === this.dayOfMonth
@@ -218,36 +211,21 @@ class MonthlyWeekDayTask extends Task {
 		this.date = nthMonthDay(newDate(this.monthDate), this.weekNumber, this.weekDay)
 	}
 
-	get lastTimeToDo() { // done
-		// Can probably optimize all this with only one loop and a smarter check to know if we are in the correct month from the start
-		let todoThisMonth = newDate()
-		todoThisMonth.setDate(1)
-		while(todoThisMonth.getDay() !== this.weekDay) {
-			todoThisMonth = addDays(todoThisMonth, 1)
+	get lastTimeToDo() {
+		const currentNthDay = nthMonthDay(newDate(), this.weekNumber, this.weekDay)
+		if (isAfter(currentNthDay, newDate())) {
+			return nthMonthDay(subMonths(newDate(), 1), this.weekNumber, this.weekDay)
+		} else {
+			return currentNthDay
 		}
-		addDays(todoThisMonth, this.weekNumber * 7)
-		if (!isSameDay(newDate(), todoThisMonth) && isAfter(todoThisMonth, newDate())) {
-			todoThisMonth.setDate(1)
-			todoThisMonth.setMonth(todoThisMonth.getMonth() - 1)
-			while(todoThisMonth.getDay() !== weekDay) {
-				todoThisMonth = addDays(todoThisMonth, 1)
-			}
-			addDays(todoThisMonth, this.weekNumber * 7)
-		}
-		return todoThisMonth
 	}
 
 	display(d) {
 		let date = newDate(d)
 		if (isAfter(this.date, date)) return false
 
-		if (this.done.some(e => isSameDay(newDate(e), date))) {
-			return true
-		} else if (!this.report) {
-			return isSameDay(date, nthMonthDay(date, this.weekNumber, this.weekDay))
-		} else if (this.isDone(this.lastTimeToDo)) {
-			return isSameDay(date, nthMonthDay(date, this.weekNumber, this.weekDay))
-		} else if ((isAfter(date, this.lastTimeToDo) || isSameDay(date, this.lastTimeToDo)) && isAfter(nthMonthDay(addMonths(this.lastTimeToDo, 1), this.weekNumber, this.weekDay), date)) {
+		const nextTimeToDo = nthMonthDay(addMonths(this.lastTimeToDo, 1), this.weekNumber, this.weekDay)
+		if (this.report && !this.isDone(this.lastTimeToDo) && (isAfterOrSame(date, this.lastTimeToDo)) && isAfter(nextTimeToDo, date)) {
 			return isSameDay(date, newDate())
 		} else {
 			return isSameDay(date, nthMonthDay(date, this.weekNumber, this.weekDay))
