696e4f489ce8140cec5e06decdd384fff621b34b
Your Name <you@example.com>
2026-05-05 20:53:38 +0200

    update libraries

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,13 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import basicSsl from '@vitejs/plugin-basic-ssl'
+import mkcert from 'vite-plugin-mkcert'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    basicSsl({
-      name: 'mmmCert',
-      domains: ['localhost:5173'],
-      certDir: '.devServer/cert'
-    })
+    mkcert()
   ],
   server: {
     https: true
