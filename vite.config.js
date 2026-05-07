6d952880504fe3e32e2a1dc0e1d7bd01975da7c6
Your Name <you@example.com>
2026-05-07 13:38:08 +0200

    add pwa behavior

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,54 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import mkcert from 'vite-plugin-mkcert'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    mkcert()
+    mkcert(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: false,
+
+      manifest: {
+        name: 'Toutdoux',
+        short_name: 'Toutdoux',
+        description: 'Toutdoux',
+        theme_color: '#121212',
+        icons: [
+          {
+            src: 'icon_64.png',
+            sizes: '64x64',
+            type: 'image/png'
+          },
+          {
+            src: 'icon_192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icon_512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'icon_512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ]
+      },
+
+      devOptions: {
+        enabled: false,
+        navigateFallback: 'index.html',
+        suppressWarnings: true,
+        type: 'module',
+      }
+    })
   ],
   base: '/Toutdoux/',
   resolve: {
